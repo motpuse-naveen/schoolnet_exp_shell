9178f0f303d347543d2cdde9d099ba3978484598
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2022-02-28 19:47:23 +0530

    shell splitter changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,43 @@
-var Activity = (function(){
-    return {
-      Init: function () {
-        $(".wrapper").css({
-          "height": window.innerHeight + "px"
-        })
-      },
-      LaunchActivity: function () {
-        $(".container-so.launch").fadeOut();
-        $(".container-so.main").show();
+var Activity = (function () {
+  return {
+    Init: function () {
+      $(".wrapper").css({
+        "height": window.innerHeight + "px"
+      })
+    },
+    LaunchActivity: function () {
+      $(".container-so.launch").fadeOut();
+      $(".container-so.main").show();
+      this.AdjustContainerHeight();
+      ScreenSplitter.InitSplitter();
+      //GuidedTour.Init();
+    },
+    AdjustContainerHeight: function () {
+      $(".wrapper").css({
+        "height": window.innerHeight + "px"
+      })
+      if ($(".container-so.main").is(":visible")) {
         var headerHt = $(".container-so.main .exp_header").outerHeight();
         var footerHt = $(".container-so.main .exp_footer").outerHeight();
         $(".exp_body_header").css({ "height": headerHt + "px" });
         $(".exp_body_footer").css({ "height": footerHt + "px" });
         var mainHt = $(".container-so.main").height();
         $(".exp_body_content").css({ "height": (mainHt - (headerHt + footerHt)) })
-  
-        SpringOscillation.LaunchActivity();
-        //GuidedTour.Init();
-        ScreenSplitter.InitSplitter();
       }
     }
-  })();
+  }
+})();
 
 $(document).ready(function () {
-    Activity.Init();
+  Activity.Init();
 });
 
 $(document).on("click", "#btn_launch", function (event) {
-    Activity.LaunchActivity();
+  Activity.LaunchActivity();
+});
+$(window).bind('orientationchange', function () {
+  this.setTimeout(function () {
+    Activity.AdjustContainerHeight();
+    ScreenSplitter.InitSplitter();
+  }, 200);
 });
